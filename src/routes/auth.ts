deba35eb690f5101abac789af2783141ac2f478e
kuruxxii <zzzihanlin@gmail.com>
2024-05-21 16:08:31 +0800

    refine auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,31 @@
-import { Router } from "express";
-import { signup, login, logout } from "../controllers/auth";
+import { Router, Request, Response, NextFunction } from "express";
+import { signup, logout } from "../controllers/auth";
 import passport from "passport";
 
 const router = Router();
 
 router.post("/signup", signup);
-router.post("/login", passport.authenticate("local"), login);
+router.post("/login", (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("local", (err: any, user: any, info: any) => {
+    if (err) {
+      // Handle error during authentication process
+      return res.status(500).json({
+        error: err.message,
+      });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        // Handle error during login process
+        res.status(500).json({
+          error: err.message,
+        });
+      }
+      // Successful authentication
+      res.status(200).json({ admin: user.username });
+    });
+  })(req, res, next);
+});
+
 router.post("/logout", logout);
 
 export default router;
