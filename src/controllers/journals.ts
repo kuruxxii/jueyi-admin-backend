e4eeb96d8d1a1e52d624f84a44c80b7d02b24755
kuruxxii <zzzihanlin@gmail.com>
2024-05-28 10:07:52 +0800

    update journals

--- src/controllers/journals.ts
+++ src/controllers/journals.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { JournalModel } from "../models/JournalModel";
+import { ArticleModel } from "../models/ArticleModel";
 
 export const getAJournal = async (req: Request, res: Response) => {
   const { vol } = req.params;
@@ -37,14 +38,22 @@ export const getPaginatedJournals = async (req: Request, res: Response) => {
 };
 
 export const addAJournal = async (req: Request, res: Response) => {
-  const { title, description, vol, coverUrl } = req.body;
+  const { title, description, vol, coverUrl, articles } = req.body;
   try {
+    for (const slug of articles) {
+      const article = await ArticleModel.findOne({ slug }).exec();
+      if (!article) {
+        return res
+          .status(400)
+          .json({ error: `slug: "${slug}" 不存在，请仔细检查` });
+      }
+    }
     const journal = await JournalModel.create({
       title,
       description,
       vol,
       coverUrl,
-      articles: [],
+      articles: articles ? articles : [],
     });
     return res.status(200).json(journal);
   } catch (error) {
@@ -68,7 +77,16 @@ export const deleteAJournal = async (req: Request, res: Response) => {
 
 export const modifyAJournal = async (req: Request, res: Response) => {
   const { vol } = req.params;
+  const { articles } = req.body;
   try {
+    for (const slug of articles) {
+      const article = await ArticleModel.findOne({ slug }).exec();
+      if (!article) {
+        return res
+          .status(400)
+          .json({ error: `slug: "${slug}" 不存在，请仔细检查` });
+      }
+    }
     const journal = await JournalModel.findOneAndUpdate(
       { vol },
       { ...req.body }
@@ -80,63 +98,5 @@ export const modifyAJournal = async (req: Request, res: Response) => {
     }
   } catch (error) {
     return res.status(400).json({ error: error.message });
-  }
-};
-
-export const addAnArticleToJournal = async (req: Request, res: Response) => {
-  const { vol } = req.params;
-  const { slug } = req.body;
-  if (slug) {
-    try {
-      const journal = await JournalModel.findOne({ vol }).exec();
-      if (!journal) {
-        return res.status(400).json("Journal does not Exist");
-      } else {
-        if (journal.articles.includes(slug)) {
-          return res
-            .status(400)
-            .json({ msg: "Article already in this journal" });
-        }
-        journal.articles.push(slug);
-        await journal.save();
-        return res.status(200).json({ msg: "Article added successfully" });
-      }
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  } else {
-    return res.status(400).json({ msg: "empty article slug" });
-  }
-};
-
-export const deleteAnArticleFromJournal = async (
-  req: Request,
-  res: Response
-) => {
-  const { vol } = req.params;
-  const { slug } = req.body;
-  if (slug) {
-    try {
-      const journal = await JournalModel.findOne({ vol }).exec();
-      if (!journal) {
-        return res.status(400).json("Journal does not Exist");
-      } else {
-        if (journal.articles.includes(slug)) {
-          journal.articles = journal.articles.filter((a) => a !== slug);
-          await journal.save();
-          return res
-            .status(200)
-            .json({ msg: "Article has been deleted from journal." });
-        } else {
-          return res
-            .status(400)
-            .json({ msg: "Article does not Exist in this journal." });
-        }
-      }
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  } else {
-    return res.status(400).json({ msg: "empty article slug" });
   }
 };
