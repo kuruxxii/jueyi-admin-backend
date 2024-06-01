af8de4bace09f37d62916a2f200e735814cf8834
kuruxxii <zzzihanlin@gmail.com>
2024-06-01 18:15:22 +0800

    UserModel

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { UserModel } from "../models/UserModel";
 
 export const addAUser = async (req: Request, res: Response) => {
-  const { email, number, startDate } = req.body;
-  const start = new Date(startDate);
-  const end = new Date(startDate);
+  const { email, number } = req.body;
+  const start = new Date();
+  const end = new Date();
   end.setFullYear(start.getFullYear() + 1); // Add one year from startDate
   end.setDate(start.getDate() + 7); // Add seven days
   try {
@@ -12,13 +12,112 @@ export const addAUser = async (req: Request, res: Response) => {
       email,
       number,
       subscription: {
-        isActive: true,
         startDate: start,
         endDate: end,
       },
     });
-    res.status(200).json(user);
+    return res.status(200).json({ msg: "用户添加成功" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const getAUser = async (req: Request, res: Response) => {
+  const { email } = req.params;
+  try {
+    const user = await UserModel.findOne({ email }).exec();
+    if (!user) {
+      return res.status(400).json({ error: "用户不存在" });
+    } else {
+      return res.status(200).json(user);
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+};
+
+export const modifyAUser = async (req: Request, res: Response) => {
+  const { email } = req.params;
+  try {
+    const user = await UserModel.findOneAndUpdate(
+      { email },
+      { ...req.body }
+    ).exec();
+    if (!user) {
+      res.status(400).json({ error: "用户不存在" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const deleteAUser = async (req: Request, res: Response) => {
+  const { email } = req.params;
+  try {
+    const user = await UserModel.findOneAndDelete({ email }).exec();
+    if (!user) {
+      res.status(400).json({ error: "用户不存在" });
+    } else {
+      res.status(200).json({ msg: "已删除该用户" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const USERS_PER_PAGE = 12;
+
+export const getFilteredUsersTotalPages = async (
+  req: Request,
+  res: Response
+) => {
+  const query: string = (req.query.query as string) || "";
+  try {
+    // Count the total number of filtered articles
+    const totalUsers = await UserModel.countDocuments({
+      $or: [
+        { email: { $regex: query, $options: "i" } },
+        { number: { $regex: query, $options: "i" } },
+      ],
+    });
+
+    // Calculate total pages
+    const totalPages = Math.ceil(totalUsers / USERS_PER_PAGE);
+
+    return res.status(200).json({ totalPages });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const getFilteredUsers = async (req: Request, res: Response) => {
+  const page: number = parseInt(req.query.page as string, 10) || 1;
+  const query: string = (req.query.query as string) || "";
+  const offset: number = (page - 1) * USERS_PER_PAGE;
+  try {
+    const users = await UserModel.aggregate([
+      {
+        $match: {
+          $or: [
+            { email: { $regex: query, $options: "i" } },
+            { number: { $regex: query, $options: "i" } },
+          ],
+        },
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+      {
+        $skip: offset,
+      },
+      {
+        $limit: USERS_PER_PAGE,
+      },
+    ]);
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
   }
 };
