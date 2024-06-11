a9a8c984ee45fdd8aea39878aa703adcbbde6b5b
kuruxxii <zzzihanlin@gmail.com>
2024-06-11 12:54:32 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app = express();
 app.use(morgan("combined"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
